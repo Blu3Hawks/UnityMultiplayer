69d63c1e931d99126b8f8d97af538b78a81ed6ea
Madar215 <galmadar2424@gmail.com>
2025-05-22 18:40:26 +0300

    Basic

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -1,9 +1,19 @@
+using System.Collections.Generic;
 using Fusion;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class LobbyManager : MonoBehaviour
+public class LobbyManager : LobbyManagerBase
 {
+    // Events
+    public event UnityAction<List<SessionInfo>> onSessionListUpdated;
+    public event UnityAction<PlayerRef, bool> onPlayersListChanged; 
+    
+    [Header("References")]
     [SerializeField] private NetworkRunner networkRunner;
+    
+    // Session list
+    private List<SessionInfo> _sessionsList = new();
 
     public void StartSession()
     {
@@ -18,5 +28,27 @@ public class LobbyManager : MonoBehaviour
     private void OnGameStarted(NetworkRunner obj)
     {
         Debug.Log("Game Started");
+    }
+    
+    public async void JoinLobby(string input)
+    {
+        var result = await networkRunner.JoinSessionLobby(SessionLobby.Custom, input);
+
+        if(result.Ok)
+            Debug.Log("Lobby Joined Successfully");
+    }
+
+    public override void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) {
+        _sessionsList = sessionList;
+        
+        onSessionListUpdated?.Invoke(_sessionsList);
+    }
+
+    public override void OnPlayerJoined(NetworkRunner runner, PlayerRef player) {
+        onPlayersListChanged?.Invoke(player, true); // When player joined - invoke with true bool
+    }
+
+    public override void OnPlayerLeft(NetworkRunner runner, PlayerRef player) {
+        onPlayersListChanged?.Invoke(player, false); // When player left - invoke with false bool
     }
 }
