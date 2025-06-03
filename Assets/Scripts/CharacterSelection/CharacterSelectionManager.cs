8896b78d8ac088000b721d119f74702366c471bf
Lior Barkai <lior5858@gmail.com>
2025-06-03 21:06:34 +0300

    Basic logic for character select

---
+++ Assets/Scripts/CharacterSelection/CharacterSelectionManager.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CharacterSelectionManager : NetworkBehaviour
+{
+    [SerializeField] private List<GameObject> characterList;
+
+    private List<int> takenIndexes = new List<int>();
+
+    private NetworkRunner networkRunner;
+
+    void Start()
+    {
+        networkRunner = NetworkRunner.GetRunnerForScene(SceneManager.GetActiveScene());
+    }
+
+
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    private void RPCRequestCharacterSelect(int index, RpcInfo info = default)
+    {
+        if (takenIndexes.Contains(index))
+        {
+            Debug.LogWarning("Character already selected");
+            return;//Insert UI logic of already selected
+        }
+        takenIndexes.Add(index);
+        RPCSpawnSelected(info.Source, index);
+
+
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void RPCSpawnSelected([RpcTarget] PlayerRef targetPlayer, int index)
+    {
+        networkRunner.SpawnAsync(characterList[Mathf.Clamp(index, 0, characterList.Count - 1)]);
+    }
+
+}
