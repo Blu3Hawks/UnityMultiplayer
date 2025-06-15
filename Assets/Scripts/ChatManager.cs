d0e9aa7bfac85d188c6745c4154570749d4034ab
Madar215 <galmadar2424@gmail.com>
2025-06-15 12:46:52 +0300

    Emotes Enums Ready

--- Assets/Scripts/ChatManager.cs
+++ Assets/Scripts/ChatManager.cs
@@ -5,28 +5,31 @@ using UnityEngine;
 public class ChatManager : NetworkBehaviour {
     [SerializeField] private EmoteUiManager emoteUiManager;
     
-    // Call this from ANY client to send an emote to exactly one player.
+    // Call this from any client to send an emote to exactly one player.
     public void SendEmoteToPlayer(EmoteType emoteType, PlayerRef target)
     {
-        // pass both the emote ID and who it's for,
-        // plus the sender (InputAuthority) so you know who sent it.
+        // Pass both the emote ID and who it's for,
+        // plus the sender (InputAuthority) to know who sent it.
         RpcReceiveEmote(emoteType, target, Object.InputAuthority);
     }
 
     [Rpc(RpcSources.All, RpcTargets.All)]
     private void RpcReceiveEmote(EmoteType emoteType, PlayerRef target, PlayerRef sender)
     {
-        // Only the intended recipient should handle this!
+        // Only the intended player should handle this
         if (Runner.LocalPlayer != target)
             return;
 
-        // Now display it in your UI:
-        // e.g. EmoteUI.Instance.ShowEmote(emote, sender);
+        // Display it in your UI:
         emoteUiManager.ShowEmote(emoteType, sender);
-        //Debug.Log($"💬 Emote {emoteId} ⬅️ from {sender}");
     }
 }
 
 public enum EmoteType {
-    
+    Smile,
+    Pressured,
+    Cry,
+    Clown,
+    Dead,
+    Cool,
 }
