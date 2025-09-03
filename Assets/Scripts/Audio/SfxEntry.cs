7d37fa0c274d3df69b6487e33dcee694b75aaa52
Madar215 <galmadar2424@gmail.com>
2025-09-03 22:49:55 +0300

    No 3D S

--- Assets/Scripts/Audio/SfxEntry.cs
+++ Assets/Scripts/Audio/SfxEntry.cs
@@ -4,7 +4,6 @@ namespace Audio {
     [CreateAssetMenu(fileName = "Sfx Entry", menuName = "Sfx Entry", order = 0)]
     public class SfxEntry : ScriptableObject {
         public SfxId id; 
-        public AudioClip clip; 
-        public bool spatial3D;
+        public AudioClip clip;
     }
 }
