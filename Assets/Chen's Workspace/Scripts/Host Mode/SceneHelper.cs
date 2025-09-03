302cc2a8fa049bfb3c711b659097e4e07d6a4dc4
Cornetto <lior5858@gmail.com>
2025-09-03 14:36:55 +0300

    Adjust scene helper

--- Assets/Chen's Workspace/Scripts/Host Mode/SceneHelper.cs
+++ Assets/Chen's Workspace/Scripts/Host Mode/SceneHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Multiplayer.Playmode;
 using UnityEngine;
 
 public class SceneHelper : MonoBehaviour
@@ -11,6 +12,13 @@ public class SceneHelper : MonoBehaviour
         #if UNITY_EDITOR
             HostGameObject.SetActive(false);
             ClientGameObject.SetActive(true);
+            if (CurrentPlayer.ReadOnlyTags().Length > 0 && CurrentPlayer.ReadOnlyTags()[0] == "Server")
+            {
+                Debug.Log("Server");
+                HostGameObject.SetActive(true);
+                ClientGameObject.SetActive(false);
+            }
         #endif
+        
     }
 }
